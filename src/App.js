ba36b6841265db9068d4bc86273ae974aa999289
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-09-15 20:03:23 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React ,{Component,Fragment} from 'react'
+import './App.css';
+
+class App extends Component {
+
+  render(){
+  return (
+  <>
+     <h1>Hello Class base Method</h1>
+                                                       <h1>Hello Class base Method</h1>
+     </>
+    
+  );
+  }
+}
+
+export default App;
