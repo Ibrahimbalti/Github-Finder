8c7d57f242964061e98d66535c53d231f29db86f
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-09-16 11:22:51 +0500

    part1 complete
    in part1 we make variable ,conditions,property,propertytypes,state,props,axios,map,componentdidmount,Refactoring class base to function base,update the state ,pass one component to another components

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 import Useritems from './Useritems';
 
 export class User extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
+  //   state = {
+  //     users: [
+  //       {
+  //         id: '1',
+  //         login: 'mojombo',
+  //         avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+  //         html_url: 'https://github.com/mojombo',
+  //       },
+  //       {
+  //         id: '2',
+  //         login: 'defunkt',
+  //         avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+  //         html_url: 'https://github.com/defunkt',
+  //       },
 
-      {
-        id: '3',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
+  //       {
+  //         id: '3',
+  //         login: 'pjhyett',
+  //         avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
+  //         html_url: 'https://github.com/pjhyett',
+  //       },
+  //     ],
+  //   };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <Useritems key={user.id} user={user} />
         ))}
       </div>
